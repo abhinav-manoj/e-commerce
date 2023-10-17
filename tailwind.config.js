892f542a28ded60ecfd3c5b76b042945bda807d2
Abhinav manoj <abhinavmanoj99@gmail.com>
2023-10-18 00:24:31 +0530

    firstCommit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        'header':'#466170',
+        'search':'#94A3B8',
+        'text-c':'#F3F4F6',
+        'nav':'#EDECEC',
+        'hero':'#D9D9D9',
+        'button':'#BD5012'
+      },
+    },
+  },
+  plugins: [],
+}
+
