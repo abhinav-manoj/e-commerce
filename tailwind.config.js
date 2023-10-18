fa2a9902281930fab4dd5d0f54fb28585753315a
Abhinav manoj <abhinavmanoj99@gmail.com>
2023-10-19 00:42:10 +0530

    checking

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    screens: {
+      
+      // => @media (min-width: 640px) { ... }
+
+      'md': '1083px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily:{
         poppin: ["Poppins", "sans-serif"],
