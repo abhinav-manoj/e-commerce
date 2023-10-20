e8314371b09b882f89b21e6142ff6a08141f62fd
Abhinav manoj <abhinavmanoj99@gmail.com>
2023-10-21 01:41:11 +0530

    hamburger

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     screens: {
-      
+      sm: "640px",
+
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
     },
     extend: {
-      fontFamily:{
+      fontFamily: {
         poppin: ["Poppins", "sans-serif"],
         roboto: ["Roboto", "sans-serif"],
-
       },
-      
-      colors:{
-        'header':'#466170',
-        'search':'#94A3B8',
-        'text-c':'#F3F4F6',
-        'nav':'#EDECEC',
-        'hero':'#D9D9D9',
-        'button':'#EE874E',
-        'button2':'#BD5012',
+
+      colors: {
+        header: "#466170",
+        search: "#94A3B8",
+        "text-c": "#F3F4F6",
+        nav: "#EDECEC",
+        hero: "#D9D9D9",
+        button: "#EE874E",
+        button2: "#BD5012",
       },
     },
   },
   plugins: [],
-}
-
+};
